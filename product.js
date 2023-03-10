da2f7c6bb3933ab7e2b160a51bb03b1625d48782
Jashanpreet20 <121371408+Jashanpreet20@users.noreply.github.com>
2023-03-10 12:05:37 +0530

    sequelize added

--- product.js
+++ product.js
@@ -1,32 +1,31 @@
-const db=require('../util/database');
 
-const Cart=require('./cart');
+const Sequelize=require('sequelize');
 
-module.exports = class Product {
-  constructor(id,title, imageUrl, description, price) {
-    this.id=id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
+const sequelize=require('../util/database');
+
+const Product=sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncreament: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  title:{
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  price:{
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  imageUrl: {
+      type: Sequelize.STRING,
+      allowNull: false
+  },
+  description:{
+    type: Sequelize.STRING,
+    allowNull: false
   }
+});
 
-  save() {
-     return db.execute('INSERT INTO product(title, price, description, imageUrl) VALUES(?, ?, ?, ?)',
-      [this.title,this.price, this.description, this.imageUrl]);
-  }
-
-  static deleteById(id){
-      return db.execute('DELETE FROM product where id= ?',[id]);
-   
-  }
-
-  static fetchAll() {
-    // this statement simply means when i call this function it will simply return the value whereever i can call
-     return db.execute('SELECT * FROM product');
-  }
-
-  static findById(id){
-    return db.execute('SELECT * FROM product where id= ?',[id]);
-  }
- };
+module.exports=Product;
